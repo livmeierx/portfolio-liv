5b4232c4b762f58da872d42646ddf924911e13a6
livmeierx <liv.meier13@gmail.com>
2022-09-22 22:45:24 -0400

    trying to deploy to github pages

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -14,9 +14,6 @@ function ProjectCards(props) {
                     <a href={props.github}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub Repo" /></a> 
                     <a href={props.deploy}><img class="project-icon" src="https://img.icons8.com/fluent/48/000000/domain.png" alt="Live Application" /></a> 
                 </p>
-                <p class="topics">
-                    ({props.topics})
-                </p>
             </div>
         </div>
     );
